fc67d783216fed72d28c0f4f86cd330326195ae1
Vlad-Kudin <wolf_od_ua@yahoo.com>
2023-08-27 22:25:13 +0300

    added Button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import { useState } from 'react';
+import Button from "./components/UI/Button";
 
 function App() {
+
+  const [isParagraphShown, setIsParagraphShown] = useState(false);
+
+  const toggleParagraph = () => {
+    setIsParagraphShown(prev => !prev);
+  };
+
   return (
     <div className="app">
       <h1>React под Капотом</h1>
+      {isParagraphShown && <p>New Paragraph</p>}
+      <Button onClick={toggleParagraph}>Показать / Скрыть</Button>
     </div>
   );
 }
